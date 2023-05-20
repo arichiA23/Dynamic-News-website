8892f7658a2d4a9f36886f54fcf8c42fcb6db846
arichiA23 <128364835+arichiA23@users.noreply.github.com>
2023-05-21 01:59:12 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -17,7 +17,7 @@ const News = (props)=>{
 
     const updateNews = async ()=> {
         props.setProgress(10);
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=589f5dccdddb4ab5b5f700c1acdf0c75&page=${page}&pageSize=${props.pageSize}`; 
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=/*your api key*/&page=${page}&pageSize=${props.pageSize}`; 
         setLoading(true)
         let data = await fetch(url);
         props.setProgress(30);
@@ -37,7 +37,7 @@ const News = (props)=>{
 
 
     const fetchMoreData = async () => {   
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=589f5dccdddb4ab5b5f700c1acdf0c75&page=${page+1}
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=/*your api key*/&page=${page+1}
         &pageSize=${props.pageSize}`;
         setPage(page+1) 
         let data = await fetch(url);
